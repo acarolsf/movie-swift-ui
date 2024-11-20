352eabb1392f587dc0fe97059ff6e41d3700598b
Ana Carolina <cscarol20@gmail.com>
2024-11-20 17:40:24 -0300

    Initial Commit

---
+++ myAppTestTests/myAppTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  myAppTestTests.swift
+//  myAppTestTests
+//
+//  Created by Ana Carolina on 20/11/24.
+//
+
+import Testing
+@testable import myAppTest
+
+struct myAppTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
